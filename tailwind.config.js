481aaad8648859a9fbd7c36a608684ef6b091b18
Mackie <zealfemi@gmail.com>
2023-05-25 20:47:12 +0100

    *

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      height: {
+        94: "22rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
